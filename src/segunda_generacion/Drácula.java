f34a03fe672c8a0a92643bbee4bc5867f8589b58
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2025-03-17 10:43:24 -0500

    Update Drácula.java

--- src/segunda_generacion/Drácula.java
+++ src/segunda_generacion/Drácula.java
@@ -1,4 +1,14 @@
 package segunda_generacion;
 
-public class Drácula {
+import primera_generacion.Vampiro;
+
+public class Drácula extends Vampiro  {
+    int estatura;
+    public Drácula(int estatura,String nombre, int edad) {
+        super(nombre, edad);
+        this.estatura=estatura;
+    }
+    public void hipnotizar(){
+        System.out.println("Venga  q' no es pa' eso...");
+    }
 }
