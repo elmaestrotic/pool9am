16a58db4d252f4f4324ec4fdfef621c7a5730cd0
elmaestrotic <45459513+elmaestrotic@users.noreply.github.com>
2025-03-17 10:44:06 -0500

    Update Main.java

--- src/primera_generacion/Main.java
+++ src/primera_generacion/Main.java
@@ -1,10 +1,20 @@
-package segunda_generacion;
+package primera_generacion;
+
+import segunda_generacion.Drácula;
+import segunda_generacion.Murcielago;
 
 public class Main {
     public static void main(String[] args) {
-        Drácula draculita = new Drácula();
-        draculita.morder();
-        draculita.chuparSangre();
-        draculita.aguantarSol();
+        Drácula draculita = new Drácula(190,"Vladimir",600);
+        draculita.hipnotizar();//método propio de Drácula
+        draculita.morder();//método heredado de Vampiro
+        draculita.chuparSangre();//método heredado de Vampiro
+        draculita.aguantarSol();//método heredado de Vampiro
+        draculita.volar();//método de la interfaz Murcielago
+        draculita.amamantar();//método de Mamifero que heredó a través de Murcielago
+        draculita.respirar();//método de Animal
+        draculita.comerFrutas();//método opcional de la interfaz Murcielago
+        draculita.vivir();//método de Servivo
+        Murcielago.hacerChichi();//método de la interfaz Murcielago
     }
 }
